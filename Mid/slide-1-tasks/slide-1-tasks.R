559edfe7f42c25797689fa3b879726760bb41070
Aftab Alam <101344790+4f74b@users.noreply.github.com>
2024-10-28 17:41:23 +0500

    .

--- Mid/slide-1-tasks/slide-1-tasks.R
+++ Mid/slide-1-tasks/slide-1-tasks.R
@@ -1,15 +1,32 @@
+# ==========================Task-1
 a=5
 b=9
 c=10
 
-# Addition
+# Task-1a
 a+b+c
 
-# Task-2
+# Task-1b
 a^2+sqrt(b)+c^3
 
-# Task-3
+# Task-1c
 (a+sqrt(b^3-c))/(a^2+b-c)
 
-# Task-4
+# Task-1d
 a*b*c
+
+# =======================Tasks-2
+v1<- c(256,144,15,196,169,15)
+v2<- c(12,66,77,45,60,50)
+
+# Task-2a
+v1+v2
+
+# Task-2b
+v1-v2
+
+# Task-2c
+v1*v2
+
+# Task-2d
+v1/v2
